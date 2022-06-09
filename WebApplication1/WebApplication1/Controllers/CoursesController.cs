b776de599c33f48b548a2db5b0066c605c150bd0
Tezot <106881132+Tezot@users.noreply.github.com>
2022-06-09 16:04:26 +0700

    Lab_TH_Tuan04

---
+++ WebApplication1/WebApplication1/Controllers/CoursesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        [Authorize]
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create()
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            Course objCourse = new Course();
+            objCourse.ListCategory = context.Categories.ToList();
+
+            return View(objCourse);
+        }
+
+        [Authorize]
+        [HttpPost]
+        
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+
+            ModelState.Remove("LecturerId");
+            if(!ModelState.IsValid)
+            {
+                objCourse.ListCategory = context.Categories.ToList();
+                return View("Create", objCourse);
+            }    
+
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerId = user.Id;
+
+            context.Courses.Add(objCourse);
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
